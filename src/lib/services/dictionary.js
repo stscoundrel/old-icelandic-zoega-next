f450fdb3cec8f1415c67efcdd339a7d5bcc09d2b
stscoundrel <sampo@pixels.fi>
2021-07-03 13:32:41 +0300

    Add dictionary service + test cases

---
+++ src/lib/services/dictionary.js
@@ -0,0 +1,65 @@
+const { getDictionary } = require('old-icelandic-zoega')
+const { VALID_AS_FIRST } = require('old-norse-alphabet')
+const { slugifyWord, slugifyLetter } = require('../utils/slugs')
+
+const addSlugs = (words) => {
+  const existingSlugs = {}
+
+  const formattedWords = words.map((word) => {
+    let slug = slugifyWord(word.word).toLowerCase()
+
+    if (existingSlugs[slug]) {
+      // Double slug, make unique.
+      existingSlugs[slug] += 1
+      slug = `${slug}-${existingSlugs[slug]}`
+    } else {
+      existingSlugs[slug] = 1
+    }
+
+    return {
+      ...word,
+      slug,
+    }
+  })
+
+  return formattedWords
+}
+
+export const getAllWords = () => {
+  const words = getDictionary()
+
+  /**
+   * Add URL safe slugs.
+   */
+  const formattedWords = addSlugs(words)
+
+  return formattedWords
+}
+
+export const getByLetter = (letter) => {
+  const words = getAllWords()
+  const byLetter = words.filter((entry) => (
+    entry.word.charAt(0).toLowerCase() === letter.toLowerCase()))
+
+  return byLetter
+}
+
+export const getWord = (word) => getAllWords().filter((entry) => entry.slug === word)[0]
+
+export const getAlphabet = () => {
+  const letters = [...VALID_AS_FIRST.filter((letter) => letter !== 'ǫ' && letter !== 'ø'), 'ö']
+
+  const formattedLetters = letters.map((letter) => ({
+    letter,
+    slug: slugifyLetter(letter),
+  }))
+
+  return formattedLetters
+}
+
+export default {
+  getAllWords,
+  getByLetter,
+  getWord,
+  getAlphabet,
+}
