85716bf610113c5ae8ced31cb8c26964a7ff43c1
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-29 23:11:43 +0300

    Migrate to Cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  defaultCommandTimeout: 15000,
+  retries: 4,
+  screenshotsFolder: 'tests/integration/logs/screenshots',
+  videosFolder: 'tests/integration/logs/videos',
+  e2e: {
+    specPattern: 'tests/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/integration/config/support.ts',
+    baseUrl: 'http://localhost:3000',
+  },
+})
