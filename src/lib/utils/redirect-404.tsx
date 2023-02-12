a0785c3ebdbc6bb2e31ffea7f1220602de33e721
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-02-12 16:20:34 +0200

    Redirect util: rewrite in TS

---
+++ src/lib/utils/redirect-404.tsx
@@ -0,0 +1,13 @@
+export interface Redirect404ResponseSchema{
+  props: object,
+  notFound: boolean,
+}
+
+export const redirect404 = (): Redirect404ResponseSchema => ({
+  props: {},
+  notFound: true,
+})
+
+export default {
+  redirect404,
+}
