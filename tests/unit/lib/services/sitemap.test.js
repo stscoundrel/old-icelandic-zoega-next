9f82a5afe56d8f77f3bbde862311f7691f3cd3b8
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-09-28 17:03:17 +0300

    Guard sitemap freshness with test assert

--- tests/unit/lib/services/sitemap.test.js
+++ tests/unit/lib/services/sitemap.test.js
@@ -2,9 +2,10 @@ import { SitemapStream, streamToPromise } from 'sitemap'
 import { hasProperty } from 'spyrjari'
 import { isArray } from 'volva'
 import { getSitemapContent, formatSitemap } from 'lib/services/sitemap'
+import { readFileSync } from 'node:fs'
 
 describe('Sitemap tests', () => {
-  process.env.NEXT_PUBLIC_SITE_URL = 'https://oldicelandic.test'
+  process.env.NEXT_PUBLIC_SITE_URL = 'https://old-icelandic.vercel.app'
   const content = getSitemapContent()
 
   test('Sitemap content is an array', () => {
@@ -23,6 +24,15 @@ describe('Sitemap tests', () => {
     const result = await formatSitemap(content, SitemapStream, streamToPromise)
 
     expect(result.includes('<?xml version="1.0" encoding="UTF-8"?>')).toBeTruthy()
-    expect(result.includes('<url><loc>https://oldicelandic.test/word/skilja</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>')).toBeTruthy()
+    expect(result.includes('<url><loc>https://old-icelandic.vercel.app/word/skilja</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>')).toBeTruthy()
+  })
+
+  test('Generated sitemap in public is up to date.', async () => {
+    // Whenever underlying data changes, the sitemap should change too.
+    // Guard this with comparison to freshly generated version.
+    const publicSitemap = readFileSync(`${__dirname}/../../../../public/sitemap.xml`).toString()
+    const result = await formatSitemap(content, SitemapStream, streamToPromise)
+
+    expect(publicSitemap).toEqual(result)
   })
 })
